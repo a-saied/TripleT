a3253a1803b70bcf9af47e70365e04f26afe9780
Ahmed Saied <asaied@umail.ucsb.edu>
2019-02-04 15:26:15 -0800

    added basic client side

--- Basics/BasicClient.java
+++ Basics/BasicClient.java
@@ -3,28 +3,100 @@ import java.net.*;
 
 public class BasicClient{
     public static void main(String[] args){
-	run(Integer.parseInt(args[0]), "localhost");
+    	BasicClient bc = new BasicClient();
+    	bc.run(Integer.parseInt(args[0]), "localhost");
     }
 
-    public static void run(int port, String hostname){
-	System.out.println("Connecting to " + hostname + "\n");
-	try{
-	    Socket client = new Socket(hostname, port);
+
+    public void run(int port, String hostname){
+		System.out.println("Connecting to " + hostname + "\n");
+		try{
+		    Socket client = new Socket(hostname, port);
 
 
-	    System.out.println("Just connected to " + client.getRemoteSocketAddress());
-	    OutputStream outToServer = client.getOutputStream();
-	    DataOutputStream out = new DataOutputStream(outToServer);
-	             
-	    out.writeUTF("Hello from " + client.getLocalSocketAddress());
-	    InputStream inFromServer = client.getInputStream();
-	    DataInputStream in = new DataInputStream(inFromServer);
-	             
-	    System.out.println("Server says " + in.readUTF());
-	    client.close();
-	}
-	catch(Exception e){
-	    e.printStackTrace();
-	}
+		    System.out.println("Just connected to " + client.getRemoteSocketAddress());
+		    OutputStream outToServer = client.getOutputStream();
+		    DataOutputStream out = new DataOutputStream(outToServer);
+		             
+		    out.writeUTF("Hello from " + client.getLocalSocketAddress());
+		    // out.writeUTF("EASY");
+		    InputStream inFromServer = client.getInputStream();
+		    // DataInputStream in = new DataInputStream(inFromServer);
+
+		    boolean makeServer = false;
+		    boolean makeClient = false;
+		    BufferedReader br = new BufferedReader(new InputStreamReader(inFromServer));
+		    while(true){
+		    	String sample = "";
+		    	sample = br.readLine();
+		    	// if(sample != ""){
+		    		System.out.println(sample);
+		    		//System.out.println("split indicator");
+		    	// }
+
+		    	if(sample.trim().equals("SERV")){
+		    		makeServer = true;
+		    		break;
+		    	}
+		    	if(sample.trim().equals("CLI")){
+		    		makeClient = true;
+		    		break;
+		    	}
+		    }
+
+		    if(makeClient) buildClient();
+		    if(makeServer) buildServer();
+		    //System.out.println("Server says " + in.readUTF());
+		    //client.close();
+		}
+		catch(Exception e){
+		    e.printStackTrace();
+		}
+    }
+
+    public void buildServer(){ 
+    	System.out.println("Creating game as host");
+    	try{
+	    	ServerSocket serv = new ServerSocket(6666);
+    		while(true){
+	    		Socket server = serv.accept();
+
+				System.out.println("Just connected to " + server.getRemoteSocketAddress());
+				DataInputStream in = new DataInputStream(server.getInputStream());
+	            
+				System.out.println(in.readUTF());
+
+				server.close();
+				break;
+			}
+    	}
+    	catch(Exception e){
+    		e.printStackTrace();
+    	}
+    }
+
+    public void buildClient(){
+    	System.out.println("Entering game as client");
+    	//Thread.sleep(500);
+    		try{
+			    Socket client = new Socket("localhost", 6666);
+
+
+			    System.out.println("Just connected to " + client.getRemoteSocketAddress());
+			    OutputStream outToServer = client.getOutputStream();
+			    DataOutputStream out = new DataOutputStream(outToServer);
+			             
+			    out.writeUTF("Hello from " + client.getLocalSocketAddress());
+			    // out.writeUTF("EASY");
+			    InputStream inFromServer = client.getInputStream();
+			    // DataInputStream in = new DataInputStream(inFromServer);
+
+			    boolean makeServer = false;
+			    boolean makeClient = false;
+			    BufferedReader br = new BufferedReader(new InputStreamReader(inFromServer));
+			}
+			catch(Exception e){
+				e.printStackTrace();
+			}
     }
 }
--- Basics/BasicServer.java
+++ Basics/BasicServer.java
@@ -4,6 +4,7 @@ import java.net.*;
 public class BasicServer {
     //class variables
     private ServerSocket serv;
+    boolean x;
 
     public static void main(String[] args){
 	BasicServer bs = new BasicServer(Integer.parseInt(args[0]));
@@ -11,11 +12,12 @@ public class BasicServer {
     }
 
     public BasicServer(int port){
+    	x = true;
 	try{
 
 
 	    serv = new ServerSocket(port);
-	    serv.setSoTimeout(10000);
+	    //serv.setSoTimeout(10000);
 	}
 	catch(Exception e){
 	    e.printStackTrace();
@@ -26,17 +28,26 @@ public class BasicServer {
 	while(true){
 	    try {
 		System.out.println("Waiting for client on port " + 
-				   serv.getLocalPort() + "...");
+				   serv.getLocalPort() + "...\n");
+		System.out.println("Waiting at INET address" + serv.getInetAddress() + ".\n");
 		Socket server = serv.accept();
-            
+
 		System.out.println("Just connected to " + server.getRemoteSocketAddress());
 		DataInputStream in = new DataInputStream(server.getInputStream());
             
 		System.out.println(in.readUTF());
 		DataOutputStream out = new DataOutputStream(server.getOutputStream());
 		out.writeUTF("Thank you for connecting to " + server.getLocalSocketAddress()
-			     + "\nGoodbye!");
-		server.close();
+			     + "\nGoodbye!\n" );
+		if(x){
+			out.writeUTF("SERV\n");
+			x = false;
+		}
+		else{
+			out.writeUTF("CLI\n");
+			x = true;
+		}
+		//server.close();
             
 	    } catch (SocketTimeoutException s) {
 		System.out.println("Socket timed out!");
