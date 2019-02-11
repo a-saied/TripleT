55385a5024c6dea60d6dfda9afdd7238d66fa0d0
Iris Xu <xuiris97@gmail.com>
2019-02-11 11:40:32 -0800

    Progress towards matching players.

--- Basics/BasicServer.java
+++ Basics/BasicServer.java
@@ -36,56 +36,80 @@ public class BasicServer {
     public void run(){
 	while(true){
 	    try {
-		System.out.println("Waiting for client on port " + 
-				   serv.getLocalPort() + "...\n");
-		System.out.println("Waiting at INET address" + serv.getInetAddress() + ".\n");
-		Socket server = serv.accept();
+			System.out.println("\nWaiting for client on port " + 
+					   serv.getLocalPort() + "...");
+			System.out.println("Waiting at INET address" + serv.getInetAddress() + ".\n");
+			Socket server = serv.accept();
 
-		System.out.println("Just connected to " + server.getRemoteSocketAddress());
-		DataInputStream in = new DataInputStream(server.getInputStream());
-            
-		System.out.println(in.readUTF());
-		DataOutputStream out = new DataOutputStream(server.getOutputStream());
-		out.writeUTF("You are now conencted to the lobby at " + server.getLocalSocketAddress()
-			     + "\nEnter your desired difficulty (Easy, Medium, Hard):" );
+			System.out.println("Just connected to " + server.getRemoteSocketAddress());
+			DataInputStream in = new DataInputStream(server.getInputStream());
+	            
+			System.out.println(in.readUTF());
+			DataOutputStream out = new DataOutputStream(server.getOutputStream());
+			out.writeUTF("You are now conencted to the lobby at " + server.getLocalSocketAddress()
+				     + "\nEnter your desired difficulty (Easy, Medium, Hard):" );
 
-		// InputStream serverInStream = server.getInputStream();
-		// Scanner serverIn = new Scanner(serverInStream);
+			// InputStream serverInStream = server.getInputStream();
+			// Scanner serverIn = new Scanner(serverInStream);
 
-		// while(true){
-		// 	if(serverIn.hasNextLine()){
-		// 		String diff = serverIn.nextLine();
-		// 		System.out.println(diff);
-		// 		break;
-		// 	}
-		// }
+			// while(true){
+			// 	if(serverIn.hasNextLine()){
+			// 		String diff = serverIn.nextLine();
+			// 		System.out.println(diff);
+			// 		break;
+			// 	}
+			// }
 
-		String diff = in.readUTF();
+			String diff = in.readUTF();
+			System.out.println("Player entered difficuly: " + diff);
+			switch(diff) {
+				case "Easy":
+					if(Easy.isEmpty()) {
+						System.out.println("Asking player to host...");
+						out.writeUTF("SERV\n");
+						Easy.add(server.getRemoteSocketAddress().toString());
+					}
+					else{
+						System.out.println("Asking player to become client...");
+						out.writeUTF("CLI " + Easy.remove(Easy.size()-1) + "\n");
+					}
+					break;
+				case "Medium":
+					if(Medium.isEmpty()) {
+						System.out.println("Asking player to host...");
+						out.writeUTF("SERV\n");
+						Medium.add(server.getRemoteSocketAddress().toString());
+					}
+					else{
+						System.out.println("Asking player to become client...");
+						out.writeUTF("CLI " + Medium.remove(Easy.size()-1) + "\n");
+					}
+					break;
+				case "Hard":
+					if(Hard.isEmpty()) {
+						System.out.println("Asking player to host...");
+						out.writeUTF("SERV\n");
+						Hard.add(server.getRemoteSocketAddress().toString());
+					}
+					else{
+						System.out.println("Asking player to become client...");
+						out.writeUTF("CLI " + Hard.remove(Easy.size()-1) + "\n");
+					}
+					break;
+				default:
+					System.out.println("Player entered invalid difficulty.");
+					out.writeUTF("Invalid difficulty entered. Disconnecting.");
+					server.close();
+			}
 
-		if(diff.equals("Easy") && Easy.isEmpty()) {
-			out.writeUTF("SERV\n");
-			Easy.add(server.getRemoteSocketAddress().toString());
-		}
-		else{
-			out.writeUTF("CLI " + Easy.remove(Easy.size()-1));
-		}
 
-		// if(x){
-		// 	out.writeUTF("SERV\n");
-		// 	x = false;
-		// }
-		// else{
-		// 	out.writeUTF("CLI\n");
-		// 	x = true;
-		// }
-		//server.close();
-            
+	            
 	    } catch (SocketTimeoutException s) {
-		System.out.println("Socket timed out!");
-		break;
+			System.out.println("Socket timed out!");
+			break;
 	    } catch (IOException e) {
-		e.printStackTrace();
-		break;
+			e.printStackTrace();
+			break;
 	    }
 	}
     }
