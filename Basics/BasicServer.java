a81cf47ecb0fbfe6d98d482e80a85ebe123eca35
Iris Xu <xuiris97@gmail.com>
2019-03-14 15:51:05 -0700

    Added proper shutdown of lobby and recording of runtime when handling client connections.

--- Basics/BasicServer.java
+++ Basics/BasicServer.java
@@ -17,7 +17,7 @@ public class BasicServer {
     public static void main(String[] args){
 
 		BasicServer bs = new BasicServer(Integer.parseInt(args[0]));
-		bs.run();
+		
     }
 
     public BasicServer(int port){
@@ -29,94 +29,122 @@ public class BasicServer {
 		avgRuntime = 0;
 		totalClients = 0;
 		try{
-
 		    serv = new ServerSocket(port);
-		    //serv.setSoTimeout(10000);
+		    acceptPlayers.start();
 		}
 		catch(Exception e){
 		    e.printStackTrace();
 		}
-    }
 
-    public void run(){
+		Scanner scn = new Scanner(System.in);
+	    String s = scn.next();
 		while(true){
-			totalClients += 1;
-			long startTime = System.nanoTime();
-		    try {
-				System.out.println("\nWaiting for client on port " + 
-						   serv.getLocalPort() + "...");
-				System.out.println("Waiting at INET address" + serv.getInetAddress() + ".\n");
-				Socket server = serv.accept();
-
-				System.out.println("Just connected to " + server.getRemoteSocketAddress());
-				DataInputStream in = new DataInputStream(server.getInputStream());
-		            
-				System.out.println(in.readUTF());
-
-				DataOutputStream out = new DataOutputStream(server.getOutputStream());
-				out.writeUTF("You are now conencted to the lobby at " + server.getLocalSocketAddress());
-
-				String address = server.getRemoteSocketAddress().toString();
-				address = address.split(":")[0];
-				address = address.split("/")[1];
-
-				String diff = in.readUTF();
-				System.out.println("Player entered difficuly: " + diff);
-				switch(diff) {
-					case "Easy":
-						if(Easy.isEmpty()) {
-							System.out.println("Asking player to host...");
-							out.writeUTF("SERV\n");
-							Easy.add(address);
-						}
-						else{
-							System.out.println("Asking player to become client...");
-							out.writeUTF("CLI " + Easy.remove(Easy.size()-1) + "\n");
-						}
-						break;
-					case "Medium":
-						if(Medium.isEmpty()) {
-							System.out.println("Asking player to host...");
-							out.writeUTF("SERV\n");
-							Medium.add(address);
-						}
-						else{
-							System.out.println("Asking player to become client...");
-							out.writeUTF("CLI " + Medium.remove(Medium.size()-1) + "\n");
-						}
-						break;
-					case "Hard":
-						if(Hard.isEmpty()) {
-							System.out.println("Asking player to host...");
-							out.writeUTF("SERV\n");
-							Hard.add(address);
-						}
-						else{
-							System.out.println("Asking player to become client...");
-							out.writeUTF("CLI " + Hard.remove(Hard.size()-1) + "\n");
-						}
-						break;
-					default:
-						System.out.println("Player entered invalid difficulty.");
-						out.writeUTF("Invalid difficulty entered. Disconnecting.");
-						server.close();
+			if ("q".equals(s)) {
+				try{
+					serv.close();
+				} catch (IOException e) {
+					System.err.println("Could not close lobby.");
+		    		System.exit(1);
+				} finally {
+		            break;
 				}
-
-		            
-		    } catch (SocketTimeoutException s) {
-				System.out.println("Socket timed out!");
-				break;
-		    } catch (IOException e) {
-				e.printStackTrace();
-				break;
-		    }
-
-		    long endTime = System.nanoTime();
-		    runtime += endTime - startTime;
-
-		    avgRuntime = (long) runtime/totalClients;
-
+			}
+			s = scn.next();
 		}
     }
+
+    private Thread acceptPlayers = new Thread() {
+    	public void run(){
+	    	Writer writer = null;
+	    	try{
+	    		writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("runtimes.txt"), "utf-8"));
+	    		while(true){
+	    			System.out.println("\nWaiting for client on port " + 
+							   serv.getLocalPort() + "...");
+					System.out.println("Waiting at INET address" + serv.getInetAddress() + ".\n");
+
+					totalClients += 1;
+					Socket server = serv.accept();
+					long startTime = System.nanoTime();
+
+					System.out.println("Just connected to " + server.getRemoteSocketAddress());
+					DataInputStream in = new DataInputStream(server.getInputStream());
+			            
+					System.out.println(in.readUTF());
+
+					DataOutputStream out = new DataOutputStream(server.getOutputStream());
+					out.writeUTF("You are now conencted to the lobby at " + server.getLocalSocketAddress());
+
+					String address = server.getRemoteSocketAddress().toString();
+					address = address.split(":")[0];
+					address = address.split("/")[1];
+
+					String diff = in.readUTF();
+					System.out.println("Player entered difficuly: " + diff);
+					switch(diff) {
+						case "Easy":
+							if(Easy.isEmpty()) {
+								System.out.println("Asking player to host...");
+								out.writeUTF("SERV\n");
+								Easy.add(address);
+							}
+							else{
+								System.out.println("Asking player to become client...");
+								out.writeUTF("CLI " + Easy.remove(Easy.size()-1) + "\n");
+							}
+							break;
+						case "Medium":
+							if(Medium.isEmpty()) {
+								System.out.println("Asking player to host...");
+								out.writeUTF("SERV\n");
+								Medium.add(address);
+							}
+							else{
+								System.out.println("Asking player to become client...");
+								out.writeUTF("CLI " + Medium.remove(Medium.size()-1) + "\n");
+							}
+							break;
+						case "Hard":
+							if(Hard.isEmpty()) {
+								System.out.println("Asking player to host...");
+								out.writeUTF("SERV\n");
+								Hard.add(address);
+							}
+							else{
+								System.out.println("Asking player to become client...");
+								out.writeUTF("CLI " + Hard.remove(Hard.size()-1) + "\n");
+							}
+							break;
+						default:
+							System.out.println("Player entered invalid difficulty.");
+							out.writeUTF("Invalid difficulty entered. Disconnecting.");
+							server.close();
+					}
+
+					long endTime = System.nanoTime();
+				    runtime += endTime - startTime;
+				    avgRuntime = (long) runtime/totalClients;
+
+				    System.out.println("Clients handled: " + totalClients);
+				    System.out.println("This runtime (ns): " + (endTime - startTime));
+				    System.out.println("Average runtime (ns): " + avgRuntime);
+
+				    writer.write(Long.toString(endTime - startTime) + "\n");
+				}
+			} catch (SocketException e) {
+					if (serv.isClosed()) {
+						System.out.println("Lobby shut down, connection closed.");
+					}
+			} catch (IOException e) {
+					e.printStackTrace();
+			} finally {
+			   		try {
+			   			writer.close();
+			   			System.out.println("Runtimes have been written to runtimes.txt");
+			   		} catch (Exception ex) {/*ignore*/}
+			}
+
+	    }
+    };
 
 }
